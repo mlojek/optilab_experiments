a84d77e8acc2f7206cb5ce85f88df87aa137bb03
mlojek <mlojek@protonmail.com>
2025-01-16 16:33:46 +0100

    enhance 003 experiment script

--- 003_knn_benchmarking/main.py
+++ 003_knn_benchmarking/main.py
@@ -1,6 +1,8 @@
 """
-Benchmarking of CMA-ES with KNN metamodel on CEC2013 benchmark.
+Benchmarking of CMA-ES with KNN metamodel on CEC2013/CEC2017 benchmark.
 """
+
+import argparse
 
 from optilab.data_classes import Bounds
 from optilab.functions.benchmarks import CECObjectiveFunction
@@ -8,10 +10,20 @@ from optilab.utils import dump_to_pickle
 from optilab.optimizers import CmaEs, KnnCmaEs
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "year",
+        type=int,
+        choices=[2013, 2017],
+        help="Year of CEC benchmark, either 2013 or 2017.",
+    )
+    parser.add_argument("dim", type=int, help="Dimensionality of benchmark functions.")
+    args = parser.parse_args()
+
     # hyperparams:
-    DIM = 10
+    DIM = args.dim
     POPSIZE = DIM * 2
-    NUM_NEIGHBORS = [n * POPSIZE for n in [2, 5, 10, 20, 30, 50]]
+    NUM_NEIGHBORS = 10
     NUM_RUNS = 51
     CALL_BUDGET = 1e4 * DIM
     TOL = 1e-8
@@ -19,21 +31,25 @@ if __name__ == "__main__":
 
     # optimized problem
     BOUNDS = Bounds(-100, 100)
-    FUNCS = [CECObjectiveFunction(2013, n, DIM) for n in range(1, 29)]
+    FUNCS = {
+        2013: [CECObjectiveFunction(2013, n, DIM) for n in range(1, 29)],
+        2017: [CECObjectiveFunction(2017, n, DIM) for n in range(1, 30)],
+    }
     TARGET = 0.0
 
-    for func in FUNCS:
-        results = []
+    for func in FUNCS[args.year]:
+        print(func.name)
 
-        # optimize using CMA-ES
         cmaes_optimizer = CmaEs(POPSIZE, SIGMA0)
-        cmaes_results = cmaes_optimizer.run_optimization(NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL)
+        cmaes_results = cmaes_optimizer.run_optimization(
+            NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL
+        )
 
-        for num_neigh in NUM_NEIGHBORS:
-            print(f'neighbors: {num_neigh}')
-            # optimize using KNN-CMA-ES
-            knn_optimizer = KnnCmaEs(POPSIZE, SIGMA0, num_neigh)
-            knn_results = knn_optimizer.run_optimization(NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL)
-            results.append(knn_results)
+        knn_optimizer = KnnCmaEs(POPSIZE, SIGMA0, NUM_NEIGHBORS)
+        knn_results = knn_optimizer.run_optimization(
+            NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL
+        )
 
-        dump_to_pickle([cmaes_results] + results, f'003_knn_benchmark_{func.name}_{DIM}.pkl')
+        dump_to_pickle(
+            [cmaes_results, knn_results], f"003_knn_benchmark_{func.name}_{DIM}.pkl"
+        )
