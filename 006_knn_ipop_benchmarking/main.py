a69ec355659c5479f5080bbb6c1852a203ec1531
mlojek <mlojek@protonmail.com>
2025-06-30 22:23:36 +0200

    Refactor 006 main function to more closely resemble 003 main function

--- 006_knn_ipop_benchmarking/main.py
+++ 006_knn_ipop_benchmarking/main.py
@@ -1,5 +1,5 @@
 """
-Benchmarking of CMA-ES with KNN metamodel on CEC2013/CEC2017 benchmark.
+Benchmarking of IPOP-CMA-ES with KNN metamodel on CEC2013/CEC2017 benchmark.
 """
 
 import argparse
@@ -7,7 +7,7 @@ import argparse
 import numpy as np
 from optilab.data_classes import Bounds
 from optilab.functions.benchmarks import CECObjectiveFunction
-from optilab.optimizers import CmaEs, IpopCmaEs, KnnCmaEs, LmmCmaEs
+from optilab.optimizers import CmaEs, IpopCmaEs, KnnCmaEs, LmmCmaEs, KnnIpopCmaEs
 from optilab.utils import dump_to_pickle
 
 if __name__ == "__main__":
@@ -41,11 +41,6 @@ if __name__ == "__main__":
         default=1,
         help="Number of concurrent processes to use.",
     )
-    parser.add_argument(
-        "--with_lmm",
-        action="store_true",
-        help="If specified, LMM-CMA-ES will also be ran.",
-    )
     args = parser.parse_args()
 
     # optimized problem
@@ -64,30 +59,30 @@ if __name__ == "__main__":
     TARGET = 0.0
 
     # hyperparams:
-    POPSIZE = 4 + np.floor(3 * np.log(DIM))
+    POPSIZE = int(4 + np.floor(3 * np.log(DIM)))
     NUM_NEIGHBORS = DIM + 2
     BUFFER_SIZES = [m * POPSIZE for m in [2, 5, 10, 20, 30, 50]]
     NUM_RUNS = 51
     CALL_BUDGET = 1e4 * DIM
     TOL = 1e-8
-    SIGMA0 = len(BOUNDS) / 2
 
     for func in FUNCS[args.year]:
         print(func.metadata.name)
         results = []
 
-        ipop_optimizer = IpopCmaEs(POPSIZE)
-        print(ipop_optimizer.metadata.name)
-        ipop_results = ipop_optimizer.run_optimization(
-            NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL, num_processes=args.num_processes
-        )
-        results.append(ipop_results)
-
-        cmaes_optimizer = CmaEs(POPSIZE, SIGMA0)
+        cmaes_optimizer = IpopCmaEs(POPSIZE)
         print(cmaes_optimizer.metadata.name)
         cmaes_results = cmaes_optimizer.run_optimization(
             NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL, num_processes=args.num_processes
         )
         results.append(cmaes_results)
+
+        for buffer_size in BUFFER_SIZES:
+            knn_optimizer = KnnIpopCmaEs(POPSIZE, NUM_NEIGHBORS, buffer_size)
+            print(knn_optimizer.metadata.name)
+            knn_results = knn_optimizer.run_optimization(
+                NUM_RUNS, func, BOUNDS, CALL_BUDGET, TOL, num_processes=args.num_processes
+            )
+            results.append(knn_results)
 
         dump_to_pickle(results, f"006_knn_benchmark_{func.metadata.name}_{DIM}.pkl")
